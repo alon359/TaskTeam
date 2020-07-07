670a3cc6a86a81533ba148cffb93bb500b3f564e
lior9631 <lior9631@gmail.com>
2020-07-07 13:02:09 +0300

    add validation to signup form

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 // Routing Module
@@ -28,6 +28,7 @@ import { SignUpComponent } from './pages/sign-up/sign-up.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/src/app/pages/sign-up/sign-up.component.ts
+++ Front/src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  signUp: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    fName: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]+')]),
+    lName: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]+')]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(8)]),
+  });
+  isWasSubmit = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // Getter form-controls
+  get fc() {
+    return this.signUp.controls;
+  }
+
+  onSignUp = () => {
+    this.isWasSubmit = true;
+    console.log('submit on');
+    console.log(this.signUp.controls);
+    console.log(('required' in this.signUp.controls.email.errors));
+  }
+
+  GetValidationClass(inputName: string) {
+    if (!this.isWasSubmit) {
+      return '';
+    } else if (this.signUp.controls[inputName].errors) {
+      return 'is-invalid';
+    } else {
+      return 'is-valid';
+    }
   }
 
 }
