8d01e3e83a7fdb962d94d03389fedd24a1e56cca
lior9631 <lior9631@gmail.com>
2020-08-17 19:41:52 +0300

    Lior upadte

--- Front/src/app/pages/home-page/home-page.component.ts
+++ Front/src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -10,6 +10,7 @@ export class HomePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
