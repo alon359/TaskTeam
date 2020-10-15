d8dd437fece107bb546bf66a39365f37cc5a1bea
lior9631 <lior9631@gmail.com>
2020-10-15 19:12:56 +0300

    update

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { OneProjectComponent } from './pages/one-project/one-project.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -9,16 +8,14 @@ import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { CurrentTasksComponent } from './pages/current-tasks/current-tasks.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { MyProjectsComponent } from './pages/my-projects/my-projects.component';
-import { OneProjectComponent } from './pages/one-project/one-project.component';
 
 const routes: Routes = [
   { path: 'tasks', pathMatch: 'full', component: CurrentTasksComponent },
   { path: 'projects', pathMatch: 'full', component: MyProjectsComponent },
-  { path: 'project', pathMatch: 'full', component: OneProjectComponent },
   { path: 'signup', pathMatch: 'full', component: SignUpComponent },
   { path: 'signIn', pathMatch: 'full', component: SignInComponent },
   { path: '', pathMatch: 'full', component: HomePageComponent },
-  { path: '**', component: NotFoundComponent }
+  { path: '**', pathMatch: 'full', component: NotFoundComponent },
 ];
 
 @NgModule({
